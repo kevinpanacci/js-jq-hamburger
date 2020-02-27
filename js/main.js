cfab01b4d0c4018ea28abc133c1aaf8a7bcf9ee9
Kevin <kevinpanacci@hotmail.it>
2020-02-27 17:36:19 +0100

    complete!

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
 // Attivazione del menu al click dell'hamburger
+
+$('.fa-bars').click(function(){
+    $('.hamburger-menu').addClass('active');
+}); 
+
+$('.close').click(function(){
+    $('.hamburger-menu').removeClass('active');
+}); // Riattivo pulsante per chiudere menu
